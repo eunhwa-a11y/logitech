7920204e858f1e11f0ba54ec98a16250b84e3600
eunhwa-a11y <ttab136@gmail.com>
2024-08-16 18:19:21 +0900

    js 파일 생성

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* HEADER */
+
+let menus = document.querySelectorAll('.top_nav > .containar > nav > ul > li');
+let topNav = document.querySelector('.top_nav')
+
+
+menus.forEach(item => {
+  item.addEventListener('mouseenter', () => {
+    let subMenuHeight = querySelector('ul').offsetHeight;
+  })
+})
